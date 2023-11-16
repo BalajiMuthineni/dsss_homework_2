509cc909a3b1666aab790b107025236fb143426b
BalajiMuthineni <150718082+BalajiMuthineni@users.noreply.github.com>
2023-11-16 20:52:58 +0100

    tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -1,30 +1,37 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import function_O, function_M, function_G
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_function_O(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
+        MINIMUM_VALUE = 1
+        MAXIMUM_VALUE = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+            rand_num = function_O(MINIMUM_VALUE, MAXIMUM_VALUE)
+            self.assertTrue(MINIMUM_VALUE <= rand_num <= MAXIMUM_VALUE)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_function_M(self):
+        # Test if random arithmetic operators are one of the specified values
+        valid_operators = ['+', '-', '*']
+        for _ in range(1000):  # Test a large number of random values
+            rand_operator = function_M()
+            self.assertIn(rand_operator, valid_operators)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_function_G(self):
+        test_cases = [
+            (5, 2, '+', 7),
+            # Add more test cases here
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+            # Example:
+            (3, 4, '*', 12),
+        ]
+
+        for num1, num2, operator, expected_result in test_cases:
+            result = function_G(num1, num2, operator)
+            self.assertEqual(result, expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
